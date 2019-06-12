e4840b2404875ec4bce6c9559241ad0da2107e93
parkyes90 <parkyes90@gmail.com>
2019-06-12 15:36:22 +0900

    add prettier, eslint setting

--- static/src/index.js
+++ static/src/index.js
@@ -1,15 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
-import { instance } from "./utils/request";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'index.css';
+import App from 'App';
+import * as serviceWorker from 'serviceWorker';
+import { instance } from 'utils/request';
 
-if (process.env.NODE_ENV === "development") {
-  instance("/csrf_token");
+if (process.env.NODE_ENV === 'development') {
+  instance('/csrf_token');
 }
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

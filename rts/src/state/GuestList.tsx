f464f24a1ee320e079c0166ceafa00bebad4ac87
parkyes90 <parkyes90@gmail.com>
2021-05-04 23:08:43 +0900

    chore: handle ref

--- rts/src/state/GuestList.tsx
+++ rts/src/state/GuestList.tsx
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 const GuestList: React.FC = () => {
+  const inputRef = useRef<HTMLInputElement>(null);
   const [name, setName] = useState("");
   const [guests, setGuests] = useState<string[]>([]);
 
@@ -9,10 +10,17 @@ const GuestList: React.FC = () => {
     setGuests([...guests, name]);
   };
 
+  useEffect(() => {
+    if (inputRef.current) {
+      console.log(inputRef.current);
+    }
+  }, [inputRef]);
+
   return (
     <div>
       <h3>Guest List</h3>
       <input
+        ref={inputRef}
         type="text"
         value={name}
         onChange={(e) => setName(e.target.value)}

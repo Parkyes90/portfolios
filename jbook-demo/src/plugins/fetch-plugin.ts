ab2da62d8dcc84e15079df043a8125409c25d300
parkyes90 <parkyes90@gmail.com>
2021-05-11 23:35:08 +0900

    refactor: extracting common caching logic

--- jbook-demo/src/plugins/fetch-plugin.ts
+++ jbook-demo/src/plugins/fetch-plugin.ts
@@ -15,7 +15,7 @@ export const fetchPlugin = (inputCode: string) => {
         };
       });
 
-      build.onLoad({ filter: /.css$/ }, async (args: any) => {
+      build.onLoad({ filter: /.*/ }, async (args: any) => {
         const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
           args.path
         );
@@ -23,7 +23,9 @@ export const fetchPlugin = (inputCode: string) => {
         if (cachedResult) {
           return cachedResult;
         }
+      });
 
+      build.onLoad({ filter: /.css$/ }, async (args: any) => {
         const { data, request } = await axios.get(args.path);
         const escaped = data
           .replace(/\n/g, "")
@@ -45,12 +47,6 @@ export const fetchPlugin = (inputCode: string) => {
       });
 
       build.onLoad({ filter: /.*/ }, async (args: any) => {
-        const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
-          args.path
-        );
-        if (cachedResult) {
-          return cachedResult;
-        }
         const { data, request } = await axios.get(args.path);
 
         const result: esbuild.OnLoadResult = {

ef79f3de091558d7843c02b46ad257cc664c6287
parkyes90 <parkyes90@gmail.com>
2021-05-15 20:12:08 +0900

    handling editor change

--- jbook-demo/src/components/code-editor.tsx
+++ jbook-demo/src/components/code-editor.tsx
@@ -3,11 +3,18 @@ import React from "react";
 
 interface CodeEditorProps {
   initialValue: string;
+  onChange(value: string): void;
 }
 
-const CodeEditor: React.FC<CodeEditorProps> = ({ initialValue }) => {
+const CodeEditor: React.FC<CodeEditorProps> = ({ initialValue, onChange }) => {
+  const onEditorDidMount = (getValue: () => string, monacoEditor: any) => {
+    monacoEditor.onDidChangeModelContent(() => {
+      onChange(getValue());
+    });
+  };
   return (
     <MonacoEditor
+      editorDidMount={onEditorDidMount}
       value={initialValue}
       theme="dark"
       height="500px"

2e5295236039ac255df2ac62f807d26e51ac7915
parkyes90 <parkyes90@gmail.com>
2021-05-17 23:17:47 +0900

    refactor: split code-cell

--- jbook-demo/src/index.tsx
+++ jbook-demo/src/index.tsx
@@ -1,45 +1,14 @@
 import "bulmaswatch/superhero/bulmaswatch.min.css";
-import React, { useState } from "react";
 import ReactDOM from "react-dom";
-import bundle from "./bundler";
-import CodeEditor from "./components/code-editor";
-import Preview from "./components/preview";
+import CodeCell from "./components/code-cell";
 
 const App = () => {
-  const [input, setInput] = useState("");
-  const [code, setCode] = useState("");
-
-  const onClick = async () => {
-    const output = await bundle(input);
-    setCode(output);
-  };
-
   return (
     <div>
-      <CodeEditor
-        initialValue="const a = 1;"
-        onChange={(value) => setInput(value)}
-      />
-      <textarea
-        value={input}
-        rows={10}
-        cols={100}
-        onChange={(e) => {
-          setInput(e.target.value);
-        }}
-      />
-      <div>
-        <button onClick={onClick}>Submit</button>
-      </div>
-      <pre>{code}</pre>
-      <Preview code={code} />
+      <CodeCell />
+      <CodeCell />
     </div>
   );
 };
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.querySelector("#root"));

efd54b05eafb4adca53c6c6afab63f01713b9a56
parkyes90 <parkyes90@gmail.com>
2021-05-24 21:39:00 +0900

    feat: Add  md editor

---
+++ jbook-demo/src/components/text-editor.tsx
@@ -0,0 +1,12 @@
+import MDEditor from "@uiw/react-md-editor";
+import React from "react";
+
+const TextEditor: React.FC = () => {
+  return (
+    <div>
+      <MDEditor.Markdown source={"# Header"} />
+    </div>
+  );
+};
+
+export default TextEditor;

1e144c8730f741587a243a2685fd9f1a1745937a
parkyes90 <parkyes90@gmail.com>
2021-05-25 23:18:27 +0900

    feat: Connect redux store

--- jbook-demo/src/index.tsx
+++ jbook-demo/src/index.tsx
@@ -1,13 +1,16 @@
 import "bulmaswatch/superhero/bulmaswatch.min.css";
 import ReactDOM from "react-dom";
-import CodeCell from "./components/code-cell";
 import TextEditor from "./components/text-editor";
+import { store } from "./state";
+import { Provider } from "react-redux";
 
 const App = () => {
   return (
-    <div>
-      <TextEditor />
-    </div>
+    <Provider store={store}>
+      <div>
+        <TextEditor />
+      </div>
+    </Provider>
   );
 };
 

11172d87e7f13733be879729a6d5fce174852e76
parkyes90 <parkyes90@gmail.com>
2021-05-26 21:59:34 +0900

    feat: Add Simple move with immer

--- jbook-demo/src/state/reducers/cellsReducer.ts
+++ jbook-demo/src/state/reducers/cellsReducer.ts
@@ -24,13 +24,21 @@ const reducer = produce((state: CellsState = initialState, action: Action) => {
     case ActionType.UPDATE_CELL:
       const { id, content } = action.payload;
       state.data[id].content = content;
-      break;
+      return;
     case ActionType.DELETE_CELL:
       delete state.data[action.payload];
       state.order = state.order.filter((id) => id !== action.payload);
-      break;
+      return;
     case ActionType.MOVE_CELL:
-      return state;
+      const { direction } = action.payload;
+      const index = state.order.findIndex((id) => id === action.payload.id);
+      const targetIndex = direction === "up" ? index - 1 : index + 1;
+      if (targetIndex < 0 || targetIndex > state.order.length - 1) {
+        return;
+      }
+      state.order[index] = state.order[targetIndex];
+      state.order[targetIndex] = action.payload.id;
+      return;
     case ActionType.INSERT_CELL_BEFORE:
       return state;
     default:

9302cb3f192c8b74c57fc052ddbabc1f1789521a
parkyes90 <parkyes90@gmail.com>
2021-05-27 22:03:51 +0900

    feat: Rendering the List of Cells

--- jbook-demo/src/components/cell-list.tsx
+++ jbook-demo/src/components/cell-list.tsx
@@ -1,9 +1,16 @@
 import React from "react";
 import { useTypedSelector } from "../hooks/use-typed-selector";
+import { CellsState } from "../state/reducers/cellsReducer";
+import CellListItem from "./cell-list-item";
 
 const CellList: React.FC = () => {
-  useTypedSelector((state) => state);
-  return <div>Cell List</div>;
+  const cellState = useTypedSelector((state) => state.cells) as CellsState;
+  const cells = cellState.order.map((id) => cellState.data[id]);
+  const renderedCells = cells.map((cell) => (
+    <CellListItem key={cell.id} cell={cell} />
+  ));
+
+  return <div>{renderedCells}</div>;
 };
 
 export default CellList;

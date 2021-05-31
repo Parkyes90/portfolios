7cc06805e14e6c8a797a8b151720da6ce3ca61a5
parkyes90 <parkyes90@gmail.com>
2021-05-31 21:02:00 +0900

    feat: Apply Add Cell components

--- jbook-demo/src/components/cell-list.tsx
+++ jbook-demo/src/components/cell-list.tsx
@@ -1,16 +1,25 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { useTypedSelector } from "../hooks/use-typed-selector";
 import { CellsState } from "../state/reducers/cellsReducer";
 import CellListItem from "./cell-list-item";
+import AddCell from "./add-cell";
 
 const CellList: React.FC = () => {
   const cellState = useTypedSelector((state) => state.cells) as CellsState;
   const cells = cellState.order.map((id) => cellState.data[id]);
   const renderedCells = cells.map((cell) => (
-    <CellListItem key={cell.id} cell={cell} />
+    <Fragment key={cell.id}>
+      <AddCell nextCellId={cell.id} />
+      <CellListItem cell={cell} />
+    </Fragment>
   ));
 
-  return <div>{renderedCells}</div>;
+  return (
+    <div>
+      {renderedCells}
+      <AddCell nextCellId={null} />
+    </div>
+  );
 };
 
 export default CellList;

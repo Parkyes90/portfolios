a365bfd5a538f232e5b6006ed8a89a887f25a6f0
parkyes90 <parkyes90@gmail.com>
2021-06-15 22:30:33 +0900

    feat: Connecting the Proxy

--- jbook/packages/local-api/src/index.ts
+++ jbook/packages/local-api/src/index.ts
@@ -1,15 +1,8 @@
 import express from "express";
-import { createProxyMiddleware } from "http-proxy-middleware";
 
 export const serve = (port: number, filename: string, dir: string) => {
   const app = express();
-  app.use(
-    createProxyMiddleware({
-      target: "http://localhost:3000",
-      ws: true,
-      logLevel: "silent",
-    })
-  );
+  app.use(express.static("../../local-client/build"));
 
   return new Promise<void>((resolve, reject) => {
     app.listen(port, resolve).on("error", reject);

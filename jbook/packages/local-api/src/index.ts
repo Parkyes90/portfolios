3adcc695e0ce14448c028ec2c3db2c599b032c13
parkyes90 <parkyes90@gmail.com>
2021-06-15 22:35:25 +0900

    feat: use path

--- jbook/packages/local-api/src/index.ts
+++ jbook/packages/local-api/src/index.ts
@@ -1,8 +1,10 @@
 import express from "express";
+import path from "path";
 
 export const serve = (port: number, filename: string, dir: string) => {
   const app = express();
-  app.use(express.static("../../local-client/build"));
+  const packagePath = require.resolve("local-client/build/index.html");
+  app.use(express.static(path.dirname(packagePath)));
 
   return new Promise<void>((resolve, reject) => {
     app.listen(port, resolve).on("error", reject);

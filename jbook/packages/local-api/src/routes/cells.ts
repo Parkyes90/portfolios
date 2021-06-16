9e21e681012dc0aacd6c980581949bf144873384
parkyes90 <parkyes90@gmail.com>
2021-06-16 20:19:25 +0900

    feat: default cell values

---
+++ jbook/packages/local-api/src/routes/cells.ts
@@ -0,0 +1,38 @@
+import express from "express";
+import fs from "fs/promises";
+import path from "path";
+interface Cell {
+  id: string;
+  content: string;
+  type: "text" | "code";
+}
+
+export const createCellsRouter = (filename: string, dir: string) => {
+  const router = express.Router();
+  const fullPath = path.join(dir, filename);
+
+  router.get("/cells", async (req, res) => {
+    try {
+      const result = await fs.readFile(fullPath, { encoding: "utf-8" });
+      res.send(JSON.parse(result));
+    } catch (err) {
+      if (err.code === "ENOENT") {
+        await fs.writeFile(fullPath, "[]", { encoding: "utf-8" });
+      } else {
+        throw err;
+      }
+    }
+  });
+
+  router.post("/cells", async (req, res) => {
+    const { cells }: { cells: Cell[] } = req.body;
+
+    await fs.writeFile(fullPath, JSON.stringify(cells), {
+      encoding: "utf-8",
+    });
+
+    res.send({ status: "ok" });
+  });
+
+  return router;
+};
